2099c549e46ea0a9bd851b5899f27d6b999ecab5
Radzell <radzell@appmunki.com>
2014-12-28 08:55:31 -0500

    fixing cache

--- app/src/production/java/com/appmunki/burritoapp/BuildDebug.java
+++
@@ -1,8 +0,0 @@
-package com.appmunki.burritoapp;
-
-/**
- * Created by radzell on 12/25/14.
- */
-public class BuildDebug {
-    public static  final String SERVER = "https://moonlit-album-658.appspot.com/_ah/api@@";
-}
--- app/src/staging/java/com/appmunki/burritoapp/BuildDebug.java
+++
@@ -1,8 +0,0 @@
-package com.appmunki.burritoapp;
-
-/**
- * Created by radzell on 12/25/14.
- */
-public class BuildDebug{
-    public static final String SERVER = "http://10.0.2.2:8080/_ah/api";
-}
